dd571d59b0b37a3fd17e4353a6176f7ec8a6e234
Jyoten Patel <jpatel@bitcompute.com>
2017-03-22 22:10:48 -0400

    Initial Commit

---
+++ MobileSurvey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileSurvey
+//
+//  Created by Jyoten Patel on 3/22/17.
+//  Copyright © 2017 Jyoten Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
